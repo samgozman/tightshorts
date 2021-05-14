ad7a162f5b043eba2a61d8046c54730e9c1b17a6
samgozman <sam@gozman.space>
2021-05-14 11:25:51 +0300

    Add stock router

--- src/app.mjs
+++ src/app.mjs
@@ -1,7 +1,10 @@
 import express from 'express'
 import webRouter from './routers/web.mjs'
+import stockRouter from './routers/stock.mjs'
 
 const app = express()
 
 app.use(webRouter)
+app.use(stockRouter)
+
 export default app
---
+++ src/routers/stock.mjs
@@ -0,0 +1,28 @@
+import express from 'express'
+import axios from 'axios'
+
+const stockRouter = new express.Router()
+
+// root index page
+stockRouter.get('/stock', async (req, res) => {
+    try {
+        const ticker = req.query.ticker
+        var config = {
+            method: 'get',
+            url: `${process.env.API_URL}/stock?ticker=${ticker}`,
+            headers: {
+                'Authorization': `Bearer ${process.env.API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const api = (await axios(config)).data
+
+        res.send(api)
+    } catch (error) {
+        res.status(404).send('Stock is not found!')
+    }
+
+})
+
+export default stockRouter
