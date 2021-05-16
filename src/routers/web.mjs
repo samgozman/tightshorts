ecec563b2019a29fed04fa0c78315b606a58b46f
samgozman <sam@gozman.space>
2021-05-16 12:28:53 +0300

    Add version property

--- src/routers/web.mjs
+++ src/routers/web.mjs
@@ -1,11 +1,16 @@
 import express from 'express'
 import fs from 'fs'
-import { join } from 'path'
-import { fileURLToPath } from 'url'
+import {
+    join
+} from 'path'
+import {
+    fileURLToPath
+} from 'url'
 
 const webRouter = new express.Router()
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const __dirname = fileURLToPath(new URL('.',
+    import.meta.url))
 const public_dir = join(__dirname, '../../public')
 
 // Setup static directory to serve
@@ -14,8 +19,11 @@ webRouter.use(express.static(public_dir))
 // root index page
 webRouter.get('', (req, res) => {
     // Save csrf token in the meta
-    const file = fs.readFileSync(join(public_dir, '/main.html'), 'utf8')
-    res.send(file.replace('{{ csrf }}', req.csrfToken()))
+    let file = fs.readFileSync(join(public_dir, '/main.html'), 'utf8')
+    file = file.replace('{{ csrf }}', req.csrfToken())
+    // App version
+    file = file.replace('{{ version }}', process.env.npm_package_version)
+    res.send(file)
 })
 
 export default webRouter
