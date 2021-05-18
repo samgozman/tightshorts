10a087d50ec1d7439284a8fc8d751a6191bd69bb
samgozman <sam@gozman.space>
2021-05-18 11:26:36 +0300

    Redirect 80 => 443

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,5 @@
 import app from './app.mjs'
+import http from 'http'
 import https from 'https'
 import fs from 'fs'
 
@@ -11,10 +12,18 @@ if (process.env.NODE_ENV === 'production') {
             ca: fs.readFileSync('/etc/letsencrypt/live/tightshorts.ru/chain.pem'),
         },
         app).listen(443, () => {
-        console.log('Server is up on port 443')
+        console.log('Prod server is up on port 443')
+    })
+
+    // Redirect from 80 to 443
+    http.createServer(function (req, res) {
+        res.writeHead(301, {
+            'Location': 'https://' + req.headers['host'] + req.url
+        })
+        res.end()
+    }).listen(80)
+} else {
+    app.listen(port, () => {
+        console.log('Dev server is up on port ' + port)
     })
 }
-
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
