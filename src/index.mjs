8d94064fbdde845a586758322b3043e0ffd38c5a
samgozman <sam@gozman.space>
2021-05-18 11:16:32 +0300

    SSL

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,19 @@
 import app from './app.mjs'
+import https from 'https'
+import fs from 'fs'
 
 const port = process.env.PORT
+
+if (process.env.NODE_ENV === 'production') {
+    https.createServer({
+            key: fs.readFileSync('/etc/letsencrypt/live/tightshorts.ru/privkey.pem'),
+            cert: fs.readFileSync('/etc/letsencrypt/live/tightshorts.ru/cert.pem'),
+            ca: fs.readFileSync('/etc/letsencrypt/live/tightshorts.ru/chain.pem'),
+        },
+        app).listen(443, () => {
+        console.log('Server is up on port 443')
+    })
+}
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
