82a78fc7c1e0e096d0c580845fde32242b741fee
samgozman <sam@gozman.space>
2021-05-18 15:58:29 +0300

    Generate CSV from table

--- web/js/table.mjs
+++ web/js/table.mjs
@@ -1,19 +1,34 @@
 // Gen table
 const table_tbody = document.getElementById('table-body')
+const array_for_csv = [
+    ['Date', 'Short Volume', 'Short Exempt Volume', 'Total Volume']
+]
 
-const createTableRow = (table, data, sv, sev, vol) => {
+const createTableRow = (table, date, sv, sev, vol) => {
     let row = table.insertRow()
-    row.insertCell(0).innerHTML = data.replace(/T(.*)/g, '')
+    row.insertCell(0).innerHTML = date
     row.insertCell(1).innerHTML = sv
     row.insertCell(2).innerHTML = sev
     row.insertCell(3).innerHTML = vol
 }
 
+const createCSVfile = (ticker) => {
+    let csvContent = 'data:text/csv;charset=utf-8,' + array_for_csv.map(e => e.join(',')).join('\n')
+    const link = document.getElementById('csv-file')
+    link.setAttribute('href', encodeURI(csvContent))
+    link.setAttribute('download', ticker + '_short_volume.csv')
+}
+
 export const generateTable = (response) => {
+    // Clear CSV array from prev
+    array_for_csv.length = 1
     const reversed_array = response.volume.reverse()
     for (const [i, el] of reversed_array.entries()) {
-        if (i < 30) createTableRow(table_tbody, el.date, el.shortVolume, el.shortExemptVolume, el.totalVolume)
+        const date = el.date.replace(/T(.*)/g, '')
+        if (i < 30) createTableRow(table_tbody, date, el.shortVolume, el.shortExemptVolume, el.totalVolume)
+        array_for_csv.push([date, el.shortVolume, el.shortExemptVolume, el.totalVolume])
     }
+    createCSVfile(response.ticker)
 }
 
 export const clearTable = () => {
