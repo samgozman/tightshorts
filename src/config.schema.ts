d32c4170d0231212283933e51e3c656300931020
samgozman <sam@gozman.space>
2022-01-05 11:39:09 +0300

    Add new env
    For ApiToken initial creation

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -5,6 +5,7 @@ export const configValidationSchema = Joi.object({
 	PORT: Joi.number().default(3001).required(),
 	API_URL: Joi.string().default('http://localhost:3000').required(),
 	API_KEY: Joi.string().required(),
+	ADMIN_SECRET: Joi.string().required(),
 	COOKIE_KEY: Joi.string().required(),
 	COOKIE_SESSION_KEY: Joi.string().required(),
 	SENTRY_DSN: Joi.string().required().uri(),
