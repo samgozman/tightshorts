872a32048bc75020a7e164b073a92395f53e9b16
samgozman <sam@gozman.space>
2022-02-05 18:30:19 +0300

    Change schema

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -4,9 +4,9 @@ import Joi from '@hapi/joi';
 export const configValidationSchema = Joi.object({
 	PORT: Joi.number().default(3001).required(),
 	API_URL: Joi.string().default('http://localhost:3000').required(),
-	API_USER_LOGIN: Joi.string().optional(),
-	API_USER_PASS: Joi.string().optional(),
-	API_KEY: Joi.string().optional(),
+	API_USER_LOGIN: Joi.string().optional().allow(null, ''),
+	API_USER_PASS: Joi.string().optional().allow(null, ''),
+	API_KEY: Joi.string().optional().allow(null, ''),
 	ADMIN_SECRET: Joi.string().required(),
 	COOKIE_KEY: Joi.string().required(),
 	COOKIE_SESSION_KEY: Joi.string().required(),
