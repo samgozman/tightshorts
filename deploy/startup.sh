6fe8c91ba8208739d29663807a3ebe7527483b22
samgozman <sam@gozman.space>
2022-02-09 08:56:43 +0300

    Add certbot setup for startup script

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -59,8 +59,31 @@ PrivateTmp=true
 WantedBy=multi-user.target
 EOL
 
-# Start nginx as a service and enable it for autostart
+# Start nginx as a service and enable it on boot
 systemctl start nginx
 systemctl status nginx
+
+# Install certbot (for certificates renewal) via snap
+# snap should be available in ubuntu 20.04 and above
+snap install core; snap refresh core
+snap install --classic certbot
+
+# Prepare the certbot command line
+ln -s /snap/bin/certbot /usr/bin/certbot
+
+# Get certificate only, without modifying existing nginx.conf
+# TODO: use variable for domain
+# --webroot - path for /.well-known
+certbot certonly --noninteractive --agree-tos \
+--cert-name tightshorts \
+-d tightshorts.ru -d www.tightshorts.ru \
+-m sam@gozman.space \
+--webroot -w /var/www/html/
+
+# Certificate auto renewal
+crontab -l > crontab_new
+echo "0 12 * * * /usr/bin/certbot renew --quiet" >> crontab_new
+crontab crontab_new
+rm crontab_new
 
 # TODO: Install docker and docker-compose
