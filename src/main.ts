dd1ec6f4acfdf270f0a7a9579d31a26cfd82ba31
samgozman <sam@gozman.space>
2022-02-19 19:04:36 +0300

    Removed unused imports

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import { create } from 'express-handlebars';
 import cookieParser from 'cookie-parser';
 import cookieSession from 'cookie-session';
-import fs from 'fs';
-import http from 'http';
-import https from 'https';
 import csurf from 'csurf';
 import helmet from 'helmet';
 import { SentryService } from '@ntegral/nestjs-sentry';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
-import { isURL } from 'class-validator';
 import { ApiConnectService } from './modules/api/api-connect.service';
 
 async function bootstrap() {
@@ -79,34 +74,5 @@ async function bootstrap() {
 	await app.init();
 
 	await app.listen(config.get('PORT'));
-
-	// Setup HTTPS connection for production server
-	// if (config.get('NODE_ENV') === 'production') {
-	// 	// https settings
-	// 	const httpsOptions: HttpsOptions = {
-	// 		key: fs.readFileSync('/etc/letsencrypt/live/tightshorts.ru/privkey.pem'),
-	// 		cert: fs.readFileSync('/etc/letsencrypt/live/tightshorts.ru/cert.pem'),
-	// 		ca: fs.readFileSync('/etc/letsencrypt/live/tightshorts.ru/chain.pem'),
-	// 	};
-
-	// 	// HTTPS server
-	// 	https.createServer(httpsOptions, server).listen(443);
-
-	// 	// Redirect from 80 to 443
-	// 	http
-	// 		.createServer(function (req, res) {
-	// 			const link = 'https://' + req.headers['host'] + req.url;
-	// 			if (isURL(link)) {
-	// 				res.writeHead(301, {
-	// 					Location: link,
-	// 				});
-	// 				res.end();
-	// 			}
-	// 		})
-	// 		.listen(80);
-	// } else {
-	// 	// http dev server
-	// 	http.createServer(server).listen(config.get('PORT'));
-	// }
 }
 bootstrap();
